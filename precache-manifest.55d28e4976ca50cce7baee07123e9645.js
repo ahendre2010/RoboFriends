5d22b4ea794a6658bcbbc8f00789aaaafe6751d2
ahendre2010 <ahendre2010@gmail.com>
2020-10-04 17:29:09 +0000

    Deploying to gh-pages from  @ df1500dea7e28b84fc21a42525cf9a7d67a9d76e 🚀

---
+++ precache-manifest.55d28e4976ca50cce7baee07123e9645.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f417ff63071054bbee075d0f666787cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ada2dc16ab11d07f8fb",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4e7f74df16257e0f4cd8",
+    "url": "/static/css/main.c421c038.chunk.css"
+  },
+  {
+    "revision": "8ada2dc16ab11d07f8fb",
+    "url": "/static/js/2.c196c29a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c196c29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7f74df16257e0f4cd8",
+    "url": "/static/js/main.b0260df7.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "022078258ba4e1947025b51b603900b0",
+    "url": "/static/media/SEGA.02207825.woff"
+  }
+]);
