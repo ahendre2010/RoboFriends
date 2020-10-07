92176a2ee428a40f3cc42b1a6138dbd77fb0756c
Akshay <ahendre2010@gmail.com>
2020-10-07 12:03:05 +0530

    redux impl completed Learning from Udemy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddelware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddelware, logger)
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
